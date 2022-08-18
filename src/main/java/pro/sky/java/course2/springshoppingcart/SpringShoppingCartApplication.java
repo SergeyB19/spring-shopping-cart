4ac3f113e5583c16424fb75a6090091fcfba7ec3
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-18 12:00:48 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/springshoppingcart/SpringShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.springshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course2/springshoppingcart/SpringShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.springshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
